3e4fbc78846c45d114b6ee68279da9c0ea452eb6
Javier Bauset <javibauset@gmail.com>
2019-05-25 19:03:08 +0200

    Comentar api para la tranquilidad de todos todos

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
+/* API from https://developers.themoviedb.org */
 export const THEMOVIEDB_API_KEY = "c5403d019b3788535cddb9234acdc8ef";
+/* Base API url */
 export const THEMOVIEDB_API_BASE_URL = "http://api.themoviedb.org/3";
+
+/* Base url for profile images */
 export const THEMOVIEDB_API_BASE_URL_IMAGES_PROFILES =
   "https://image.tmdb.org/t/p/w185";
-export const THEMOVIEDB_API_BASE_URL_IMAGES_POPSTERS =
+/* Base url for posters images */
+export const THEMOVIEDB_API_BASE_URL_IMAGES_POSTERS =
   "https://image.tmdb.org/t/p/w185";
+/* Base url form movie info */
+export const THEMOVIEDB_BASE_URL_MOVIE = "https://www.themoviedb.org/movie/";
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,14 @@ import axios from "axios";
 import { stringify } from "querystring";
 import { THEMOVIEDB_API_BASE_URL, THEMOVIEDB_API_KEY } from "../config";
 
+/**
+ * Qet the query string from a map of parameters and adds the API_KEY.
+ *
+ * Example of use: {with_people: [1, 2]} => ?api_key=...&with_people=1%2C2
+ * 
+ * @param {values} values Object with pairs of values.
+ * @return {String} The query string.
+ */
 function getQuerySring(values) {
   return (
     "?" + stringify(Object.assign({ api_key: THEMOVIEDB_API_KEY }, values))
@@ -9,12 +17,23 @@ function getQuerySring(values) {
 }
 
 const ApiService = {
+  /**
+   * Get a list with the 20 most popular actors and actresses.
+   *
+   * @return {Object} An object with the results in data.results[*]
+   */
   async getPersonPopular() {
     return await axios.get(
       `${THEMOVIEDB_API_BASE_URL}/person/popular${getQuerySring()}`
     );
   },
 
+  /**
+   * Get a list with the movies in which the given people have participated.
+   *
+   * @param {Array} people Array with the ids of the people.
+   * @return {Object} An object with the results in data.results[*]
+   */
   async getMoviesFromPeople(people) {
     let params = { with_people: people.join(",") };
     console.log(params);
